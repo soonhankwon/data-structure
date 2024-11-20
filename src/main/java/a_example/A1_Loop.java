afa467aa424b34aafb8f6a604aabf6471c313b5b
soonhankwon <soonable@naver.com>
2024-11-20 22:35:16 +0900

    Effective Loop

--- src/main/java/a_example/A1_Loop.java
+++ src/main/java/a_example/A1_Loop.java
@@ -1,2 +1,27 @@
-package a_example;public class A1_Loop {
+package a_example;
+
+public class A1_Loop {
+    public static void main(String[] args) {
+        loopVer1();
+        loopVer2();
+    }
+
+    private static void loopVer1() {
+        int number = 2;
+        // execute 100times
+        while(number <= 100) {
+            System.out.println(number);
+            number++;
+        }
+    }
+
+    // more effective
+    private static void loopVer2() {
+        int number = 2;
+        // execute 50times
+        while(number <= 100) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
 }
